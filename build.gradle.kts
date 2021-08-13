b1d6a447106950027ef2e2e3c5477235accff7ba
Dominic Zirbel <dominiczirbel@gmail.com>
2021-08-13 13:13:20 -0700

    Convert root build.gradle and settings.gradle to Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext.kotlin_version = "1.5.21"
     repositories {
         google()
         mavenCentral()
     }
+
     dependencies {
-        classpath "com.android.tools.build:gradle:4.2.1"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:4.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
     }
 }
 
@@ -24,7 +23,7 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven { url "https://jitpack.io" }
+        maven { setUrl("https://jitpack.io") }
         jcenter()
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "Thumbprint"
-include ':thumbprint'
+
+include(":thumbprint")
