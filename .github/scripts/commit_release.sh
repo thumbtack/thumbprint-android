a1ca2d697b9f5b11a2866fc3a8cf4151b96dff25
Mallika Potter <mallika@thumbtack.com>
2021-08-23 22:02:56 -0700

    Uncomment line

--- .github/scripts/commit_release.sh
+++ .github/scripts/commit_release.sh
@@ -17,4 +17,4 @@ sed -i "" -E "s/version *= *(["'"'"'])(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9]
 git add --all
 git commit -m "Release $1"
 git push https://$GITHUB_TOKEN@github.com/thumbtack/thumbprint-android.git head:$(git branch --show-current)
-#gh pr create --title "Release $1" --body "" --head $(git branch --show-current)
+gh pr create --title "Release $1" --body "" --head $(git branch --show-current)
